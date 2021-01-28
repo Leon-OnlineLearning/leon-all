09e95ce0c808f14a373a88a121f9193973aa1fba
abdo ragab hashem <ar1813@fayoum.edu.eg>
2021-01-28 17:33:02 +0200

    update only the cert with script

--- certificate/create_cert.sh
+++ certificate/create_cert.sh
@@ -5,13 +5,15 @@
 ######################
 
 # Generate private key
-openssl genrsa -des3 -out myCA.key 2048
+# openssl genrsa -des3 -out myCA.key 2048
 # Generate root certificate
-openssl req -x509 -new -nodes -key myCA.key -sha256 -days 825 -out myCA.pem
+# openssl req -x509 -new -nodes -key myCA.key -sha256 -days 825 -out myCA.pem
 
 ######################
 # Create CA-signed certs
 ######################
+localIP=192.168.18.19 #use your ip here
+publicIP=156.209.166.123 # use public ip if you want
 
 NAME=leon-test.com # Use your own domain name
 # Generate a private key
@@ -27,8 +29,12 @@ subjectAltName = @alt_names
 [alt_names]
 DNS.1 = $NAME # Be sure to include the domain name here because Common Name is not so commonly honoured by itself
 DNS.2 = bar.$NAME # Optionally, add additional domains (I've added a subdomain here)
-IP.1 = 192.168.18.19 # Optionally, add an IP address (if the connection which you have planned requires it)
+IP.1 = $localIP  # Optionally, add an IP address (if the connection which you have planned requires it)
+IP.2 = $publicIP 
 EOF
 # Create the signed certificate
 openssl x509 -req -in $NAME.csr -CA myCA.pem -CAkey myCA.key -CAcreateserial \
 -out $NAME.crt -days 825 -sha256 -extfile $NAME.ext
+
+
+# openssl verify -CAfile myCA.pem -verify_hostname bar.mydomain.com mydomain.com.crt
