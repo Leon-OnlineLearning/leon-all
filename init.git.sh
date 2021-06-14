a386b47b6e568f14c5bd0b891a6502a60199d704
abdo hashem <ar1813@fayoum.edu.eg>
2021-06-14 23:41:47 +0200

    update init script

--- init.git.sh
+++ init.git.sh
@@ -1,6 +1,6 @@
 git clone  git clone git@github.com:Leon-OnlineLearning/leon-all.git
 git submodule init
-git submodule update
+git submodule update --init --recursive
 mkdir nginx/logs
 touch nginx/cert/fullchain.pem
 touch nginx/cert/privkey.pem
